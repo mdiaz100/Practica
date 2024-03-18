69fb5e585b93e0e6ae9d527bff8aa315c476ab26
mdiaz100 <124297233+mdiaz100@users.noreply.github.com>
2024-03-17 20:16:50 -0500

    ...

--- Taller2/Menu.py
+++ Taller2/Menu.py
@@ -1,2 +1,9 @@
-#Esto es una prueba de l hacer esto
-class Prueba
+class Menu: #O(n)
+
+    def orden(): #O(1)
+        question = [
+            inquirer.List('respuesta',
+                        message="¿Importa el orden?",
+                        choices=['Sí', 'No'],
+                    ),
+        ]  # O(1) 
